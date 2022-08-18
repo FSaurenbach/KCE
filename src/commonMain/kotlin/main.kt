8a962e1893cccea58dd986ad05ed32b3cbf6785b
Ferdinand Saurenbach <ferdinand161@outlook.de>
2022-08-18 08:36:13 +0100

    Update main.kt

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -18,7 +18,7 @@ suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"
 class MyScene : Scene() {
 	override suspend fun SContainer.sceneMain() {
 		val minDegrees = (-16).degrees
-		val maxDegrees = (+16).degrees
+		val maxDegrees = (+30).degrees
 
 		val image = image(resourcesVfs["korge.png"].readBitmap()) {
 			rotation = maxDegrees
